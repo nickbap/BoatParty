5c92790cd116422377f120d378a42eec371fc0b0
Nick Bautista <nbautista12@gmail.com>
2019-12-18 21:56:36 -0800

    Removed unnecessary flask-bootstrap import

--- boatparty/__init__.py
+++ boatparty/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
 from flask_pagedown import PageDown
 from flask_mail import Mail
 from flask_moment import Moment
