19e307bf68779bf2b0146707803eb3230bdd2b57
Nick Bautista <nbautista12@gmail.com>
2019-12-29 20:02:29 -0800

    Revised SQLALCHEMY_DATABASE_URI config

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or 'I_Solemnly_Swear_Im_Up_To_No_Good'
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI') or 'sqlite:///boat_party.db'
+        'SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'boat_party.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
     MAIL_PORT = os.environ.get('MAIL_PORT') or 587
