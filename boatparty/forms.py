e3acb4dfe6fa535e44d36a8d2742def0a216eb22
Nick Bautista <nbautista12@gmail.com>
2019-12-30 14:16:42 -0800

    Added better validation for posts from same user

--- boatparty/forms.py
+++ boatparty/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from flask_pagedown.fields import PageDownField
+from boatparty.models import GuestBookPost
 
 
 class GuestBookForm(FlaskForm):
     name = StringField('Your Name', validators=[DataRequired()])
     pagedown = PageDownField('Please leave us a note!\n Markdown supported!')
     submit = SubmitField('Good to Go!')
+
+    def validate_name(self, name):
+        name = GuestBookPost.query.filter_by(name=name.data).first()
+        if name:
+            raise ValidationError(
+                "Looks like you've left us a note already! Let's save some space for others!")
