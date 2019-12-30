dec618bf401a14c029d0f7f5bc145a9dcbc4837a
Nick Bautista <nbautista12@gmail.com>
2019-12-29 21:29:09 -0800

    Added db and GuestBookPost model to flask shell

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
-from boatparty import create_app
+from boatparty import create_app, db
+from boatparty.models import GuestBookPost
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'GuestBookPost': GuestBookPost}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
