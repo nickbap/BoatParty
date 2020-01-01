d19af5289b0f4a05863ffa250b147302661d1d06
Nick Bautista <nbautista12@gmail.com>
2020-01-01 14:10:12 -0800

    Revised logic in get_gallery_photos to grab only jpgs

--- boatparty/utils.py
+++ boatparty/utils.py
@@ -35,4 +35,4 @@ def get_countdown_data():
 def get_gallery_photos():
     """Get file names for photos in static/img/gallery"""
     gallery_dir = os.path.join(current_app.root_path, 'static/img/gallery')
-    return [g for g in sorted(os.listdir(gallery_dir))]
+    return [g for g in sorted(os.listdir(gallery_dir)) if '.jpg' in g]
