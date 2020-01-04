f588598b4c0a446403c0087c84c460a4935b2aff
Nick Bautista <nbautista12@gmail.com>
2020-01-03 19:41:26 -0800

    Recreated migration repo to handle mysql unique text issues

--- boatparty/models.py
+++ boatparty/models.py
@@ -8,8 +8,8 @@ class GuestBookPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     posted_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    post_md = db.Column(db.Text, unique=True, nullable=False)
-    post_html = db.Column(db.Text, unique=True, nullable=False)
+    post_md = db.Column(db.Text, nullable=False)
+    post_html = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return '<GuestBookPosts {} {} {}>'.format(self.id, self.posted_at, self.name)
